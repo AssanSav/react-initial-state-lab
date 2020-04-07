7a5b16f2bed331479d2073240ff4bcaa00c29392
AssanSav <assane.savadogo81@gmail.com>
2020-04-07 12:39:44 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
-// your Bomb code here!
+import React, { Component } from "react"
+
+export default class Bomb extends Component {
+
+    constructor({ initialCount }) {
+        super()
+        this.state = {
+            secondsLeft: initialCount
+        }
+    }
+
+    render() {
+        console.log(this.state.secondsLeft)
+        if (this.state.secondsLeft === 0) {
+           return (
+                <p>Boom!</p>    
+            )  
+        } else {
+            console.log(this.state.secondsLeft)
+            return (
+                <p>{ this.state.secondsLeft } seconds left before I go boom!</p>   
+            )  
+        }   
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,23 @@
-// your ImageSlider code here!
+import React, { Component } from "react"
+
+
+export default class ImageSlider extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    render() {
+        if (this.state.currentSlideIndex === 0) {
+            return (
+                <div>
+                    I am on slide {this.state.currentSlideIndex}
+                </div>
+            )
+        }
+    
+    }
+}
